7c9f99ddb5f909ce52568eb8fa93df58116e89cd
Carlitxa <cmoutinho191@gmail.com>
2026-06-23 17:41:08 +0100

    resolver problemas

--- AventuraSaudável/Assets/Scripts/Carla/PlayerJump.cs
+++ AventuraSaudável/Assets/Scripts/Carla/PlayerJump.cs
@@ -5,20 +5,38 @@ public class PlayerJump : MonoBehaviour
     public float jumpForce = 8f;
 
     private Rigidbody2D rb;
-    private Animator animator;
+    private bool estaNoChao = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Mantém o gato na mesma posição horizontal
+        transform.position = new Vector3(
+            -3f,
+            transform.position.y,
+            transform.position.z
+        );
+
+        if (Input.GetKeyDown(KeyCode.Space) && estaNoChao)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            animator.SetTrigger("Jump");
+            rb.linearVelocity = new Vector2(
+                rb.linearVelocity.x,
+                jumpForce
+            );
+
+            estaNoChao = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Chao"))
+        {
+            estaNoChao = true;
         }
     }
 }
